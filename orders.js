aeaeb45b6c606d36bc7d52ec69300575ff38454f
fauzanrizkyanag <fauzanrizkyana@gmail.com>
2022-09-04 09:05:43 +0700

    project baru

---
+++ orders.js
@@ -0,0 +1,53 @@
+const Orders = [
+    {
+        productName: 'Keripik Daun Kelor',
+        flavor: 'Keju',
+        productNumber: '0001',
+        paymentStatus: 'Due',
+        shipping: 'pending'
+    },
+
+    {
+        productName: 'Keripik Daun Kelor',
+        flavor: 'BBQ',
+        productNumber: '0002',
+        paymentStatus: 'Refunded',
+        shipping: 'Decliened'
+    },
+    {
+        productName: 'Keripik Daun Kelor',
+        flavor: 'Pedas',
+        productNumber: '0003',
+        paymentStatus: 'Due',
+        shipping: 'pending'
+    },
+
+    {
+        productName: 'Keripik Daun Kelor',
+        flavor: 'Asin',
+        productNumber: '0004',
+        paymentStatus: 'due',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Keripik Daun Kelor',
+        flavor: 'Keju',
+        productNumber: '0001',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Keripik Daun Kelor',
+        flavor: 'BBQ',
+        productNumber: '0002',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Keripik Daun Kelor',
+        flavor: 'Pedas',
+        productNumber: '0003',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+]
